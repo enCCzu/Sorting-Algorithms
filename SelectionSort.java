e768e58aae45d1e945ab526862d1926f9591e9a2
Erin Zhou <92766077+enCCzu@users.noreply.github.com>
2021-10-20 16:20:23 -0400

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,19 @@
+public static void sort(int[] arr){
+  int smallestInt, temp; 
+  
+  // One by one move boundary of unsorted part of the array
+  for (int i = 0; i < arr.length - 1; i++){
+    
+    // Find the minimum element in unsorted part of the array
+    smallestInt = i; 
+    for (int j = i + 1; j < arr.length; j++){
+      if (arr[j] < arr[smallestInt]){
+        smallestInt = j; 
+        // swap the smallest element in the unsorted part of the array with the first element in the unsorted part of the array
+        temp = arr[smallestInt]; 
+        arr[smallestInt] = arr[i]; 
+        arr[i] = temp; 
+      }
+    }
+  }
+}
